21c39ec286daf35f52c48e5d55514c4baf24680c
Gabriel Marques <bielmarques922@gmail.com>
2020-06-24 15:52:56 -0300

    Aula 14 - Métodos de Objetos

---
+++ js/aula14.js
@@ -0,0 +1,25 @@
+/*  Aula 14 - Métodos de Objetos */
+ 
+
+/* 01 - Metodos basicos */ 
+console.log("Funcionamento dos Metodos: ");
+
+var aluno = {
+  'nome': 'Maria',
+  'sobrenome': 'Pereira',
+  'ano_nasc': 1992,
+  'nome_completo': function () {
+    var n_completo = this.nome + " " + this.sobrenome;
+    return n_completo;
+  },
+  'ver_idade': function() {
+    var idade = 2020 - this.ano_nasc;
+    return idade; 
+  }
+};
+
+console.log( aluno.nome_completo() );
+
+console.log('A idade é: ' + aluno.ver_idade() );
+
+console.log("---------------------------------");
